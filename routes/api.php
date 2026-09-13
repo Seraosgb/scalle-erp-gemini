7e602ac1fe00eb383096eec15b15bca141687360
Bruno Soares <serapsgb@gmail.com>
2026-09-13 19:25:58 -0300

    fix(rh): corrige erro 500 no carregamento do portal do colaborador ajustando prioridade de rotas

--- routes/api.php
+++ routes/api.php
@@ -304,6 +304,9 @@ Route::middleware(['auth:sanctum', IdentifyTenant::class, CheckSubscriptionStatu
     Route::get('/rh/holerites', [HoleriteController::class, 'index']);
     Route::get('/rh/holerites/{id}', [HoleriteController::class, 'show']);
     Route::post('/rh/holerites', [HoleriteController::class, 'store']);
+    // 1º AS ROTAS ESPECÍFICAS DO PORTAL DO COLABORADOR
+    Route::get('/rh/holerites/meus', [App\Http\Controllers\Api\HoleriteController::class, 'meusHolerites']);
+    Route::get('/rh/holerites/meus/{id}/pdf', [App\Http\Controllers\Api\HoleriteController::class, 'baixarMeuPdf']);
 
     // Rotas do Recrutamento
     Route::get('/rh/vagas', [RecrutamentoController::class, 'indexVagas']);
